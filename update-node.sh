a45bc601eb5d55d1762ca10b324ab755320b7333
ndlovun <kndlovun@gmail.com>
2023-01-13 08:54:36 -0500

    changing shell script

--- update-node.sh
+++ update-node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/node-project && git pull
-/home/ec2-user/node-project && npm install
+cd /home/ec2-user/node-project && npm install
 cd /home/ec2-user/node-project && npm run start
